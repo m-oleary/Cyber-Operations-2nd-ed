8c5b59dd96be9d606c8b33fc49d5021acb44a051
Mike O'Leary <moleary@towson.edu>
2018-10-10 22:08:31 -0400

    Rather than using indices, it makes more sense to search by the name in the tag

--- Chapter 10/10-9 SysmonExtract.ps1
+++ Chapter 10/10-9 SysmonExtract.ps1
@@ -1,22 +1,28 @@
 # PowerShell script to parse the Sysmon Operational log and examine the data from a 
 #  process creation event by casting the data as XML.
 
-$events = Get-WinEvent -FilterHashTable @{logname='Microsoft-Windows-Sysmon/Operational'; id =1}
+$events = Get-WinEvent -FilterHashTable @{logname='Microsoft-Windows-Sysmon/Operational'; id=1}
 
 foreach($event in $events) {
   
    $eventXML = [xml]$event.ToXml()
 
-   $image = $eventXML.Event.EventData.Data[3].'#text'
-   $commandline = $eventXML.Event.EventData.Data[4].'#text'
-   $user = $eventXML.Event.EventData.Data[6].'#text'
-   $integritylevel = $eventXML.Event.EventData.Data[10].'#text'
-   $eventpid = $eventXML.Event.EventData.Data[2].'#text'
-   $eventppid = $eventXML.Event.EventData.Data[13].'#text'
-   $parent = $eventXML.Event.EventData.Data[14].'#text'
+   $image = $eventXML.Event.EventData.Data | where {$_.name -eq "Image"}
+   $commandline = $eventXML.Event.EventData.Data | 
+        where {$_.name -eq "CommandLine"}
+   $user = $eventXML.Event.EventData.Data | where {$_.name -eq "User"}
+   $integritylevel = $eventXML.Event.EventData.Data | 
+        where {$_.name -eq "IntegrityLevel"}
+   $eventpid = $eventXML.Event.EventData.Data | 
+        where {$_.name -eq "ProcessID"}
+   $eventppid = $eventXML.Event.EventData.Data | 
+        where {$_.name -eq "ParentProcessID"}
+   $parent = $eventXML.Event.EventData.Data | 
+        where {$_.name -eq "ParentImage"}
 
    $image, $commandline, $user, $integritylevel, $eventpid, $eventppid, $parent
 
    ""
 }
+
 
